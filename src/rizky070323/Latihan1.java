14ff493208fda52bddbd95cc3766799f5236dcc6
muhammadrizky99 <rizky.btr99@gmail.com>
2023-03-08 11:37:29 +0700

    Initial commit

---
+++ src/rizky070323/Latihan1.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rizky070323;
+
+/**
+ *
+ * @author LABSIKOMP03
+ */
+public class Latihan1 {
+    public static void main(String[] args){
+        int number = 10;
+        char letter = 'a';
+        boolean result = true;
+        String str = "hello";
+        
+        System.out.println(number);
+        System.out.println(letter);
+        System.out.println(result);
+        System.out.println(str);
+    }
+}
